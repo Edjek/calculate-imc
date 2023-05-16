7f90d2c91b48e9ff1685bdc4b8dbdd7b692bd66e
Edjek <edjek@hotmail.fr>
2023-05-17 01:23:48 +0200

    css

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,22 @@
-console.log('coucou');
+const formImc = document.forms.formImc
+
+formImc.addEventListener('submit', function (e) {
+    e.preventDefault()
+    const size = formImc.size.value
+    const weight = formImc.weight.value
+
+    const result = calculateImc(weight, size)
+    createElement('div', 'message', `Votre imc est de ${result}`, formImc, 'beforeend')
+})
+
+function calculateImc(weight, size) {
+    const result = weight / Math.pow(size / 100, 2)
+    return result
+}
+
+function createElement(elem, className, text, elemParent, position) {
+    const messageResult = document.createElement(elem)
+    messageResult.classList.add(className)
+    messageResult.textContent = text
+    elemParent.insertAdjacentElement(position, messageResult)
+}
